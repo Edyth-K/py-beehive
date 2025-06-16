393ac3ac8160bf41417a5cf7dc014aed5ff4aa50
Kelvin Dela Cruz <kelvin.dcruz@gmail.com>
2025-06-16 13:23:55 -0500

    import map objects with pytmx

--- survivor/main.py
+++ survivor/main.py
@@ -2,6 +2,7 @@ from settings import *
 import time
 from player import Player
 from sprites import *
+from pytmx.util_pygame import load_pygame
 
 from random import randint
 
@@ -18,16 +19,20 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
+        # map
+        self.setup()
+
         # sprites
         self.player = Player((WINDOW_WIDTH/2, WINDOW_HEIGHT/2), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(50,100), randint(50,100)
-            CollisionSprite((x, y), (w,h), (self.all_sprites, self.collision_sprites))
+
+
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
 
     def run(self):
-        
-        
         while self.running:
             dt = self.clock.tick(120) / 1000
 
