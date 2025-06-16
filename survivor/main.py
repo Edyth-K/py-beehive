e3379d9e2563d0ee0d686a75cfbb3b23d47d4fb6
Kelvin Dela Cruz <kelvin.dcruz@gmail.com>
2025-06-15 20:58:01 -0500

    initial game loop in game class, add player class with sprite that moves with input

--- survivor/main.py
+++ survivor/main.py
@@ -1,1 +1,66 @@
 from settings import *
+import time
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups, pos):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('assets', 'images', 'player','down', '0.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = pos)
+        self.speed = 500
+        self.direction = pygame.math.Vector2(0,0)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+        if keys[pygame.K_s]:
+            self.direction.y = 1
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+class Game:
+    def __init__(self):
+        
+        pygame.init()
+
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Survivor-like")
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        self.all_sprites = pygame.sprite.Group()
+        self.player = Player(self.all_sprites, (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+    def run(self):
+        
+        
+        while self.running:
+            dt = self.clock.tick(120) / 1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+        
+            # update
+            self.all_sprites.update(dt)
+
+            # draw
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
+
+            pygame.display.update()
+
+            time.sleep(0.001)
+
+        pygame.quit()
+
+Game().run()
