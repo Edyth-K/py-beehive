41ce12f414d9d6fd39f2ba54039f1c491153dcc9
Kelvin Dela Cruz <kelvin.dcruz@gmail.com>
2025-06-24 11:26:03 -0500

    initial commit for pong

---
+++ pong/sprites.py
@@ -0,0 +1,15 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, groups):
+        super().__init__(groups)
+
+        # image
+        self.image = pygame.Surface(SIZE['paddle'])
+
+        # rect & movement
+        self.rect = self.image.get_frect(center = POS['player'])
+
+class Ball(pygame.sprite.Sprite):
+    pass
